604917da6e7cac52b70e2932bd47d137973e1292
Sonali <sonalisinghblore@gmail.com>
2022-10-07 15:28:58 +0530

    Api to demo usage of query parameters.

--- rest/src/main/java/com/restdemo/rest/user/controller/UserController.java
+++ rest/src/main/java/com/restdemo/rest/user/controller/UserController.java
@@ -3,15 +3,28 @@ package com.restdemo.rest.user.controller;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("users") //http://localhost:8080/users
+@RequestMapping("users") //http://localhost:8080/users?page=1&limit=50
+
+
 /*
-   Class to demo @PathVariable
+   Class to demo @RequestParam
  */
 public class UserController {
 
     @GetMapping(path="/{userId}")
     public String getUser(@PathVariable String userId) {
         return "get user was called with the userId: "+userId;
+    }
+
+    /**
+     * Method to read the query parameters page and limit.
+     * @param page
+     * @param limit
+     * @return
+     */
+    @GetMapping
+    public String getUser(@RequestParam(value="page") int page, @RequestParam(value="limit") int limit) {
+        return "get user was called with the page: "+page + " and limit: " +limit;
     }
 
     @PostMapping
